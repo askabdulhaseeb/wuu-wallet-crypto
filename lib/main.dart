2c93cc94027164dcaa9868ff115d9f5970e8683d
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-06-22 17:06:36 +0500

    apply theme and lint rule with some dependecies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:muuwallet/providers/app_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +10,23 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Muu Wallet',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
+    return MultiProvider(
+      // ignore: always_specify_types
+      providers: [
+        ChangeNotifierProvider<AppThemeProvider>.value(
+          value: AppThemeProvider(),
+        ),
+      ],
+      child: Consumer<AppThemeProvider>(
+          builder: (BuildContext context, AppThemeProvider theme, _) {
+        return MaterialApp(
+          title: 'Muu Wallet',
+          theme: AppThemes.light,
+          darkTheme: AppThemes.dark,
+          themeMode: theme.themeMode,
+          home: const Scaffold(),
+        );
+      }),
     );
   }
 }
---
+++ lib/providers/app_theme.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class AppThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode {
+    if (_themeMode == ThemeMode.system) {
+      final Brightness brightness =
+          SchedulerBinding.instance.window.platformBrightness;
+      return brightness == Brightness.dark;
+    } else {
+      return _themeMode == ThemeMode.dark;
+    }
+  }
+
+  void toggleTheme(bool isOn) {
+    _themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
+class AppThemes {
+  static const Color _primary = Color.fromRGBO(8, 167, 255, 1);
+  static const Color _secondary = Color.fromRGBO(2, 122, 190, 1);
+  static final ThemeData dark = ThemeData(
+    scaffoldBackgroundColor: Colors.black,
+    primaryColor: _primary,
+    iconTheme: const IconThemeData(color: Colors.white),
+    dividerTheme: const DividerThemeData(color: Colors.grey, thickness: 0.5),
+    listTileTheme: const ListTileThemeData(
+      contentPadding: EdgeInsets.symmetric(horizontal: 16),
+      horizontalTitleGap: 10,
+    ),
+    colorScheme: const ColorScheme.dark(
+      primary: _primary,
+      secondary: _secondary,
+    ),
+  );
+
+  static final ThemeData light = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: _primary,
+    iconTheme: const IconThemeData(color: Colors.black),
+    dividerTheme: const DividerThemeData(color: Colors.grey, thickness: 0.5),
+    listTileTheme: const ListTileThemeData(
+      contentPadding: EdgeInsets.symmetric(horizontal: 16),
+      horizontalTitleGap: 10,
+    ),
+    colorScheme: const ColorScheme.light(
+      primary: _primary,
+      secondary: _secondary,
+    ),
+  );
+}
