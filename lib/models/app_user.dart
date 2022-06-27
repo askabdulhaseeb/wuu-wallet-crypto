d61f70568b63f96244e49080e406caa10a77d773
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-06-27 18:31:10 +0500

    Create app_user.dart

---
+++ lib/models/app_user.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppUser {
+  AppUser({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.imageURL,
+  });
+
+  final String uid;
+  final String name;
+  final String email;
+  final String imageURL;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'uid': uid,
+      'name': name,
+      'email': email,
+      'imageURL': imageURL,
+    };
+  }
+
+  // ignore: sort_constructors_first
+  factory AppUser.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return AppUser(
+      uid: doc.data()!['uid'] ?? '',
+      name: doc.data()!['name'] ?? '',
+      email: doc.data()!['email'] ?? '',
+      imageURL: doc.data()!['imageURL'] ?? '',
+    );
+  }
+}
