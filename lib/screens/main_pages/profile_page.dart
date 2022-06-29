8792b382f64cc485ed46228b39fe69c2ba89724c
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-06-29 21:34:07 +0500

    Update profile_page.dart

--- lib/screens/main_pages/profile_page.dart
+++ lib/screens/main_pages/profile_page.dart
@@ -4,8 +4,10 @@ import 'package:provider/provider.dart';
 
 import '../../providers/user_provider.dart';
 import '../../widget/custom_widgets/circular_profile_image.dart';
+import '../../widget/custom_widgets/custom_elevated_button.dart';
 import '../../widget/custom_widgets/title_clickable_tile.dart';
 import '../../widget/profile/user_profile_info_card.dart';
+import '../setting_screen/setting_screen.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -17,7 +19,9 @@ class ProfilePage extends StatelessWidget {
         title: const Text('Account'),
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () => Navigator.of(context).pushNamed(
+              SettingScreen.rotueName,
+            ),
             icon: const Icon(Icons.settings),
           ),
         ],
@@ -48,6 +52,19 @@ class ProfilePage extends StatelessWidget {
                   ),
                   onTap: () {},
                 ),
+                const SizedBox(height: 10),
+                CustomElevatedButton(
+                  title: 'Logout',
+                  bgColor: Colors.transparent,
+                  padding: const EdgeInsets.symmetric(vertical: 18),
+                  textStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 18,
+                  ),
+                  border: Border.all(color: Theme.of(context).primaryColor),
+                  onTap: () {},
+                ),
+                const SizedBox(height: 60),
               ],
             ),
           ),
