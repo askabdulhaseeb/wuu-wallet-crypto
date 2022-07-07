5d87db5e15f0da6aa664d5e10c20be6d78463833
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-07-08 02:25:18 +0500

    Update app_theme.dart

--- lib/providers/app_theme.dart
+++ lib/providers/app_theme.dart
@@ -32,6 +32,7 @@ class AppThemes {
     appBarTheme: const AppBarTheme(
       color: Colors.transparent,
       elevation: 0,
+      centerTitle: true,
       titleTextStyle: TextStyle(color: Colors.white),
       iconTheme: IconThemeData(color: Colors.white),
     ),
@@ -56,6 +57,7 @@ class AppThemes {
     appBarTheme: const AppBarTheme(
       color: Colors.transparent,
       elevation: 0,
+      centerTitle: true,
       titleTextStyle: TextStyle(color: Colors.black),
       iconTheme: IconThemeData(color: Colors.black),
     ),
