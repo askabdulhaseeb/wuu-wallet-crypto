5c0f8e43544797cf0340bd2888267f96d19508be
askabdulhaseeb <askabdulhaseeb@gmail.com>
2022-07-28 02:49:00 +0500

    add models

--- lib/models/app_user.dart
+++ lib/models/app_user.dart
@@ -1,34 +1,44 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
 
 class AppUser {
   AppUser({
     required this.uid,
     required this.name,
     required this.email,
+    required this.username,
     required this.imageURL,
   });
 
   final String uid;
   final String name;
   final String email;
+  final String username;
   final String imageURL;
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'uid': uid,
+      // 'uid': uid,
       'name': name,
       'email': email,
-      'imageURL': imageURL,
+      'user_name': username,
+      // 'imageURL': imageURL,
     };
   }
 
   // ignore: sort_constructors_first
-  factory AppUser.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+  factory AppUser.fromMap(Map<String, dynamic> map) {
     return AppUser(
-      uid: doc.data()!['uid'] ?? '',
-      name: doc.data()!['name'] ?? '',
-      email: doc.data()!['email'] ?? '',
-      imageURL: doc.data()!['imageURL'] ?? '',
+      uid: map['data']['uid'] ?? '',
+      name: map['data']['name'] ?? '',
+      email: map['data']['email'] ?? '',
+      username: map['data']['user_name'] ?? '',
+      imageURL: map['data']['imageURL'] ?? '',
     );
   }
+
+  String toJson() => json.encode(toMap());
+
+  // ignore: sort_constructors_first
+  factory AppUser.fromJson(String source) =>
+      AppUser.fromMap(json.decode(source));
 }
